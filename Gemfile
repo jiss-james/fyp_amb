0e25787645a26622fce3ab5af576467d91a9ca11
Jiss James <jissjames360@gmail.com>
2017-01-04 11:29:32 +0000

    Search wokring

--- Gemfile
+++ Gemfile
@@ -46,6 +46,8 @@ gem 'bootstrap', '~> 4.0.0.alpha5'
 
 gem 'cancancan'
 
+gem 'ransack', github: 'activerecord-hackery/ransack'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,16 @@ Rails.application.routes.draw do
 
 
 
+  get 'search', to: 'search#results'
+
   devise_for :users
+  resources :users, :only => [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :companies do
     resources :activities
   end
-  
+
   root to: "home#index"
 
   get 'page/about'
